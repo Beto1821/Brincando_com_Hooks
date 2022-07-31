b3f80b4c0cbbc5c45441f07a5e6e736c29bcc65c
beto1821@uol.com.br <Katia@1006>
2022-07-31 20:03:42 -0300

    inser;cão de usuarios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [ repositories, setRepositories ] = useState([
-     { id: 1, name:  'repo-1' },
-     { id: 2, name:  'repo-2' },
-     { id: 3, name:  'repo-3' },
-  ]);
+  const [ repositories, setRepositories ] = useState([]);
+  const [ user, setUser ] = useState('');
 
-  function handleAddRepository() {
-    setRepositories([...repositories, { id: Math.random, name: 'novorep'}])
+    useEffect(() => {
+  async function fetchData(user) {
+    const response = await fetch('https://api.github.com/users/${user}/repos');
+    const data = await response.json();
+    setRepositories(data);
   }
+  fetchData();
+    }, []);
+
+    useEffect(() => {
+      const filtered = repositories.filter(repo => repo.favorite);
+
+      document.title = `You have ${filtered.length} favorited`;
+
+    }, [repositories])
+
+    function handleUser({ target }) {
+    const { value } = target;
+    setUser(value);
+  };
+
+    function handleFavorite(id) {
+      const newRepositories = repositories.map(repo => {
+        return repo.id === id ? {...repo, favorite: !repo.favorite } : repo
+      });
+      setRepositories(newRepositories);
+    }
 
   return (
     <div>
-      <ul>
-        {repositories.map(repo => (
-          <li key={repo.id}>{repo.name}</li>
-          ))}
-      </ul>
-          <button onClick={handleAddRepository}>
-            Adicionar repositório
-          </button>
+      <input 
+      type="string"
+      onChange={handleUser}
+      placeholder="Usuario GIthub"
+      />
+        <ul>
+          {repositories.map(repo => (
+            <li key={repo.id}>
+              {repo.name}
+              {repo.favorite && <span>(Favorite)</span> }
+              <button onClick={() => handleFavorite(repo.id)}>Favorite</button>
+            </li>
+            ))}
+        </ul>
+
     </div>
   )
 }
